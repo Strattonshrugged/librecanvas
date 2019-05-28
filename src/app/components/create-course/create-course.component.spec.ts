024030ca85fee0522af6e8d3b5fa2cdb266f77a9
jared stratton <jared.stratton@student.centralia.edu>
2019-05-28 13:42:46 -0700

    test setup problem

--- src/app/components/create-course/create-course.component.spec.ts
+++ src/app/components/create-course/create-course.component.spec.ts
@@ -22,4 +22,21 @@ describe('CreateCourseComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should validate the form', () => {
+    expect(component.createCourseForm.invalid).toBeTruthy();
+
+    // expect form object to be invalid
+
+    // put in values to make it valid
+
+    // expect form object to be valid
+
+    // put in bad email value (ex. greg@ )
+
+    // expect form object to be invalid (or expect form field to be invalid)
+
+    //expect();
+    //component.createCourseForm.get('coursetitle').setValue('Jared')
+  });
 });
